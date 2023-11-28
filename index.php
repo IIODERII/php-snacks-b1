22b55f48049494b0cae11ec58d585b1a337930e6
IIODERII <oderrisi@gmail.com>
2023-11-28 15:12:56 +0100

    fixed get with post in snack bonus

--- index.php
+++ index.php
@@ -83,8 +83,8 @@ if (isset($_GET['name']) && isset($_GET['mail']) && isset($_GET['age'])) {
 
 //$paragraph = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a";
 
-if (isset($_GET['paragraph'])) {
-    $paragraph = $_GET['paragraph'];
+if (isset($_POST['paragraph'])) {
+    $paragraph = $_POST['paragraph'];
     $paragraphList = explode('.', $paragraph);
 }
 ?>
@@ -146,7 +146,7 @@ if (isset($_GET['paragraph'])) {
             <div class="w-50 bg-warning overflow-auto">
                 <h1>Snack bonus</h1>
                 <div class="container py-5">
-                    <form class='text-start' action='index.php' method="GET">
+                    <form class='text-start' action='index.php' method="POST">
                         <div class="mb-3">
                             <label for="paragraph" class="form-label">Your paragraph</label>
                             <textarea type="text" class="form-control" id="paragraph" name='paragraph'
